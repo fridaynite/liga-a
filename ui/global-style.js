7330e304f8254a9652e6bd9d8529d3759a061b13
Леонид <getit7040@gmail.com>
2021-03-01 01:38:55 +0300

    duplicate css property was removed

--- ui/global-style.js
+++ ui/global-style.js
@@ -2,13 +2,11 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
   * {
-      box-sizing: border-box;
-    
+    box-sizing: border-box;   
     }
     
   body {
     font-family: sans-serif;
     background-color: rgb(231, 231, 231);
-    box-sizing: border-box;
 }
 `
